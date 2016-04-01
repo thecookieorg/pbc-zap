387723260f0509ddd4392440ea1f45ec2602c1d3
Marko Manojlovic <marko@thecookieapps.com>
2016-04-01 00:38:10 +0000

    Created pages controller, added bootstrap theme

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'pages/index'
+
   devise_for :users
+  
+  root 'pages#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
